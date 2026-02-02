edb7f6aad35e7f9fd8fb092ec9bfcb9cf20d3d81
Luc Sarrazin <luc.sarrazin@free.fr>
2026-02-02 19:09:08 +0100

    Add LucScene and started coding CameraMovement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,7 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CameraMovement : MonoBehaviour
 {
+    private InputSystem_Actions myActions;
+    [SerializeField] private float moveSpeed = 10f;
+    private const float edge_threshold = 0.45f;
+
+    private void OnEnable()
+    {
+        myActions = new InputSystem_Actions();
+        myActions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        myActions.Player.Disable();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +28,21 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 mousePos = GetMousePosition();
+        Vector2 screenUV = new Vector2(mousePos.x / Screen.width - 0.5f, mousePos.y / Screen.height - 0.5f);
         
+        Vector3 move = Vector3.zero;
+        if (screenUV.x < -edge_threshold) move.x = -1f;
+        if (screenUV.x > edge_threshold) move.x = 1f;
+        if (screenUV.y < -edge_threshold) move.y = -1f;
+        if (screenUV.y > edge_threshold) move.y = 1f;
+
+        move = Quaternion.Euler(0f, transform.eulerAngles.y, 0f) * move.normalized;
+        transform.Translate(Time.deltaTime * moveSpeed * move, Space.World);
+    }
+
+    private Vector2 GetMousePosition()
+    {
+        return myActions.Player.Mouse.ReadValue<Vector2>();
     }
 }
