d946a484b2f353ef69e1f8e3a913fa53cd841651
Luc Sarrazin <luc.sarrazin@free.fr>
2026-02-17 17:31:44 +0100

    Add Cursor

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CursorController : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public Texture2D cursorClickedTexture;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        changeCursor(cursorTexture);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        mousePosition.z = Camera.main.transform.position.z;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, mousePosition, Color.red);
+        
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.collider.name);
+        }
+    }
+
+    private void changeCursor(Texture2D cursorType)
+    {
+        Vector2 hotspot = new Vector2(cursorType.width / 2, cursorType.height / 2);
+        Cursor.SetCursor(cursorTexture, hotspot, CursorMode.Auto);
+    }
+}
